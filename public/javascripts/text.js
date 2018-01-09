5713c312559d1c408a71a95874445d92b9817f19
Robert Mausolf <rmausolf06@gmail.com>
2018-01-09 21:18:28 +0000

    minor adjustment

--- public/javascripts/text.js
+++ public/javascripts/text.js
@@ -1,7 +1,28 @@
-
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
 
 $(document).ready(function() {
   $('#jBold').click(function() {
     document.execCommand('bold');
   });
+  $('#jItalic').click(function() {
+    document.execCommand('italic');
+  });
+  $('#jUnderline').click(function() {
+    document.execCommand('underline');
+  });
 });
+
+// document.getElementById("form").onsubmit = function(e){
+//     e.preventDefault();
+//     var el = document.getElementById("editDescription");
+//     el.value = document.getElementById("fake_textarea").innerHTML;
+//     document.getElementById("form").submit();
+// }
