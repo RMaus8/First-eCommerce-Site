3e3fafae21de64fbe133b2109fd48d26fe24effd
Robert Mausolf <rmausolf06@gmail.com>
2018-10-02 17:48:21 +0000

    fix app error

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
-// seedDB();
+seedDB();
 
 app.use(session({
     secret: "Zola and Milo are my special dogs",
--- seeds.js
+++ seeds.js
@@ -100,28 +100,28 @@ function seedDB(){
             console.log(err);
         } else {
             console.log("removed products");        
-            data.forEach(function(seed){
-                Product.create(seed, function(err, campground){
-                    if(err){
-                        console.log(err);
-                    } else {
-                        console.log("added a product");
-                        // Comment.create(
-                        //     {
-                        //         text: "this place is great",
-                        //         author: "Homer"
-                        //     }, function(err, comment){
-                        //         if(err) {
-                        //             console.log(err);
-                        //         } else {
-                        //             campground.comments.push(comment);
-                        //             campground.save();
-                        //             console.log("created new comment");
-                        //         }
-                        //     });
-                    }
-                });        
-            });
+            // data.forEach(function(seed){
+            //     Product.create(seed, function(err, campground){
+            //         if(err){
+            //             console.log(err);
+            //         } else {
+            //             console.log("added a product");
+            //             // Comment.create(
+            //             //     {
+            //             //         text: "this place is great",
+            //             //         author: "Homer"
+            //             //     }, function(err, comment){
+            //             //         if(err) {
+            //             //             console.log(err);
+            //             //         } else {
+            //             //             campground.comments.push(comment);
+            //             //             campground.save();
+            //             //             console.log("created new comment");
+            //             //         }
+            //             //     });
+            //         }
+            //     });        
+            // });
         }
     });
 
